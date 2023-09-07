f5e1d6a6bc9d3a568089bc4f56af48c5e2e4275e
Imily22 <azeimily2001@gmail.com>
2023-09-07 09:09:57 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ * Return: alawys 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
